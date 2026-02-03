0de814f4294fa50d1fb71cc65554386680ea8e60
Eduardo Marchi <edu.3e@hotmail.com>
2026-02-03 11:34:27 -0300

    Cap. 26 Aula 01

---
+++ desafio016/login.py
@@ -0,0 +1,28 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+class MeuServidor(BaseHTTPRequestHandler):
+    def do_GET(self):
+        caminho_arquivo = os.path.join(os.path.dirname(__file__), 'index.html') # mudar o nome do arquivo no caminho para reaproveitar este servidor http em outros exercícios
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+        with open(caminho_arquivo, 'rb') as arquivo:
+            self.wfile.write(arquivo.read())
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(b'<h1>Os dados foram enviados!</h1>')
+
+
+server = HTTPServer(('localhost', 8000), MeuServidor)
+print("Servidor rodando em http://localhost:8000")
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    print("Encerrando servidor...")
+    server.server_close()
